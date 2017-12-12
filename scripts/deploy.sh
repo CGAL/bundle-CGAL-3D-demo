18aebdf37758e1f5e2fe8d0e0c50a6fca4c5bba5
Laurent Rineau <laurent.rineau@cgal.org>
2017-12-12 16:00:31 +0100

    Update the image for recent Polyhedron demo
    fixup! for "WIP AppRun works but not appimage"

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,14 +10,10 @@ export PATH
 [ -d /dist/usr/lib ] || mkdir -p /dist/usr/lib
 [ -d /results ] || mkdir /results
 cd /build/demo/Polyhedron
-find -name '*.so' | xargs -l10 cp --preserve=mode --parents -t /dist/usr/lib
+find -name '*.so' -a -not -name '*plugin*.so' | xargs -l10 cp --preserve=mode --parents -t /dist/usr/lib
 cp --preserve=mode Polyhedron_3 /dist/usr/bin/
-cp -r --preserve=mode Plugins/*/*.so /dist/usr/bin/
+cp -r --preserve=mode --parents implicit_functions/*.so /dist/usr/bin/
+cp -r --preserve=mode --parents Plugins/*/*.so /dist/usr/bin/
 linuxdeployqt /dist/usr/bin/Polyhedron_3  $(find -name '*plugin*.so' -printf ' -executable=/dist/usr/bin/%p') -bundle-non-qt-libs -verbose=2 -no-translations
-rm -f /dist/AppRun
-printf '#!/bin/sh\n\ncd ./usr/bin/\nLD_LIBRARY_PATH=$PWD:$PWD/../lib;export LD_LIBRARY_PATH;$PWD/Polyhedron_3\n $@\n' > /dist/AppRun
-chmod a+x /dist/AppRun
-cp --preserve=mode -a /scripts/cgal_logo_ipe_2013.png /dist/default.png
-cp --preserve=mode -a /scripts/default.desktop /dist/
-cd /results
-appimagetool -v /dist
+$SHELL -$- /scripts/create_appimage.sh
+
