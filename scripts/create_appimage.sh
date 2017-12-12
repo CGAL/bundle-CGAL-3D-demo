f912195742a94a3dc80d16115d3060d26d5ec659
Laurent Rineau <laurent.rineau@cgal.org>
2017-12-12 16:05:51 +0100

    Fix a SELinux error

--- scripts/create_appimage.sh
+++ scripts/create_appimage.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 set -e
-cp --preserve=mode -a /scripts/cgal_logo_ipe_2013.png /dist/default.png
-cp --preserve=mode -a /scripts/default.desktop /dist/
+cp /scripts/cgal_logo_ipe_2013.png /dist/default.png
+cp /scripts/default.desktop /dist/
 cd /results
 appimagetool -v /dist
