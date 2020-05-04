cf1675642e83e0ce93371bb6beb500cd8f8578eb
Maxime Gimeno <maxime.gimeno@gmail.com>
2020-05-04 15:53:50 +0200

    don't call -j if NUMBER_OF_CORES is empty

--- scripts/docker_entrypoint.sh
+++ scripts/docker_entrypoint.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-/scripts/build.sh -j$NUMBER_OF_DEDICATED_CORES
+if [ -n "$-j$NUMBER_OF_DEDICATED_CORES" ]; then
+  /scripts/build.sh -j$NUMBER_OF_DEDICATED_CORES
+else
+  /scripts/build.sh
+fi
 /scripts/deploy.sh
