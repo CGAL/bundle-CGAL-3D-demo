a3540ec0fe112d94585db18658a16ce8eb8991bf
Laurent Rineau <laurent.rineau@cgal.org>
2020-05-04 16:11:32 +0200

    Fix the test on NUMBER_OF_DEDICATED_CORES

--- scripts/docker_entrypoint.sh
+++ scripts/docker_entrypoint.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-if [ -n "$-j$NUMBER_OF_DEDICATED_CORES" ]; then
-  /scripts/build.sh -j$NUMBER_OF_DEDICATED_CORES
-else
-  /scripts/build.sh
-fi
+/scripts/build.sh -j${NUMBER_OF_DEDICATED_CORES:-1}
 /scripts/deploy.sh
